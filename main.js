7872ee2b0a73b796cdacdd07d83378239ac65d7c
HackDev <80620538+Hackx2@users.noreply.github.com>
2022-07-17 08:26:46 +0100

    shiz

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+const meow = "HackTheAir#1098"
+const id = nickname
+
+var img = document.createElement("img");
+
 function copy() {
-    navigator.clipboard.writeText("HackTheAir#1098");
+    navigator.clipboard.writeText(meow)
+    document.querySelector('#nickname').textContent = 'copied!!';
+    src="icons/gamebanana.png"
+    setTimeout(function(){
+        document.querySelector('#nickname').textContent = 'Discord';
+        src="icons/gamebanana.png"
+        img.src="icons/discord.svg"
+    }, 1000);
 } // ngl this is kinda gay
+
+function change() {
+    src="icons/gamebanana.png"
+    document.querySelector('#nickname').textContent = 'copied!!';
+    
+}
+
+function href(name) {
+    location.href = name
+}
+
+function shiz(){
+    href="https://twitter.com/AnimatesHack"
+}
+
