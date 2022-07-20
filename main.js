a48ff2e83598a62ed8d278e5519a8391b23dfd96
HackDev <80620538+Hackx2@users.noreply.github.com>
2022-07-20 10:46:48 +0100

    piggy mad sus

--- main.js
+++ main.js
@@ -2,21 +2,26 @@ const nicknamE = document.getElementById('nickname');
 var nickname;
 window.onload = () => {
     var r = new XMLHttpRequest();
-    r.open('GET', 'https://discard.ralsin.ml/704037343878971424.json');
+    r.open('GET', 'https://raw.githubusercontent.com/Hackx2/Website/main/707228048864444477.json');
     r.onload = () => {
         var res = JSON.parse(r.response)
-        document.getElementById('dpfp').setAttribute('src', 'https://cdn.discordapp.com/avatars/' + res.id + '/' + res.avatar + '?size=1024&quality=lossless');
-        document.getElementById('dpfp').removeAttribute('style');
         nickname = res.username + '#' + res.discriminator;
         nicknamE.innerText = nickname;
     }
     r.send();
 }
-function href(name) {
-    location.href = name
-}
-function copy() {
+function meow() {
     setTimeout(() => nicknamE.innerText = nickname, 2e3)
     navigator.clipboard.writeText(nickname)
     nicknamE.innerText = 'Copyed :D'
 }
+
+function href(name) {
+    location.href = name
+}
+
+
+
+
+
+
