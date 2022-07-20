81414b5adbe771c32182e278a657f33cc902eca4
HackDev <80620538+Hackx2@users.noreply.github.com>
2022-07-20 08:34:41 +0100

    e

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-const meow = "HackTheAir#1098"
-const id = nickname
-
-var img = document.createElement("img");
-
-function copy() {
-    navigator.clipboard.writeText(meow)
-    document.querySelector('#nickname').textContent = 'copied!!';
-    src="icons/gamebanana.png"
-    setTimeout(function(){
-        document.querySelector('#nickname').textContent = 'Discord';
-        src="icons/gamebanana.png"
-        img.src="icons/discord.svg"
-    }, 1000);
-} // ngl this is kinda gay
-
-function change() {
-    src="icons/gamebanana.png"
-    document.querySelector('#nickname').textContent = 'copied!!';
-    
+const nicknamE = document.getElementById('nickname');
+var nickname;
+window.onload = () => {
+    var r = new XMLHttpRequest();
+    r.open('GET', 'https://discard.ralsin.ml/704037343878971424.json');
+    r.onload = () => {
+        var res = JSON.parse(r.response)
+        document.getElementById('dpfp').setAttribute('src', 'https://cdn.discordapp.com/avatars/' + res.id + '/' + res.avatar + '?size=1024&quality=lossless');
+        document.getElementById('dpfp').removeAttribute('style');
+        nickname = res.username + '#' + res.discriminator;
+        nicknamE.innerText = nickname;
+    }
+    r.send();
 }
-
 function href(name) {
     location.href = name
 }
-
-function shiz(){
-    href="https://twitter.com/AnimatesHack"
+function copy() {
+    setTimeout(() => nicknamE.innerText = nickname, 2e3)
+    navigator.clipboard.writeText(nickname)
+    nicknamE.innerText = 'Copyed :D'
 }
-
